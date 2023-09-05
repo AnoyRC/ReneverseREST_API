d5750178c268af5085405d13fb7fe41703595daa
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2023-09-05 17:00:49 +0530

    Webhook for Readme.io

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const readme = require("readmeio");
 
 const app = express();
 
@@ -13,6 +14,20 @@ app.use("/api/stg/user", require("./routes/api/stg/user"));
 
 app.use("/api/game", require("./routes/api/game"));
 app.use("/api/user", require("./routes/api/user"));
+
+const secret = "CDnDMoYE7QGcSQchx4MuSPumpSEObyJq";
+
+app.post("/webhook", async (req, res) => {
+  const signature = req.headers["readme-signature"];
+
+  try {
+    readme.verifyWebhook(req.body, signature, secret);
+  } catch (e) {
+    return res.status(401).json({ error: e.message });
+  }
+
+  return res.json({});
+});
 
 const PORT = process.env.PORT || 8081;
 
